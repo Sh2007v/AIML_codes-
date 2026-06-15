048dfa3503bb4f9e97a5a14920b89d4d86a5c201
Sh2007v <virdisha50@gmail.com>
2026-06-15 23:50:35 +0530

    add

--- java.java
+++ java.java
@@ -2,6 +2,25 @@ public class java{
     public static void main(){
         System.out.println("Shah");
         System.out.println("Virdi");
+        System.out.print("Singh");
+        System.out.print(" Baaz");
+        String c = "Good";
+        System.out.println(c);
+        final int mynum = 90;
+        System.out.println(mynum);
+        int u = 6;
+        int i = 8;
+        System.out.println("The sum is:"+u+i);
+        System.out.println("The sum is:" +(u+i));
+        char myG = 'A';
+        System.out.println(myG);
+        // var x; // Error
+        var x = 5;  // OK
+        // x = 9.99;   // Error - can't assign a double to an int
+        System.out.println(x);
     }
 }
 //This is my first Java code.
+/* I am learning Java to become a 
+Backend and 
+    Full Stack developer */
