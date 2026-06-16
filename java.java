b252ca790b18a586c06fde540b3a6d83d17c6f66
Sh2007v <virdisha50@gmail.com>
2026-06-16 19:26:29 +0530

    k

--- java.java
+++ java.java
@@ -1,6 +1,6 @@
 public class java{
     public static void main(){
-        System.out.println("Shah");
+        /* System.out.println("Shah");
         System.out.println("Virdi");
         System.out.print("Singh");
         System.out.print(" Baaz");
@@ -18,9 +18,169 @@ public class java{
         var x = 5;  // OK
         // x = 9.99;   // Error - can't assign a double to an int
         System.out.println(x);
-    }
+        System.out.println(2+5);
+        String String1 = "Shahbaaz";
+        String String2 = "Singh";
+        System.out.println(String1+ " " + String2);
+        String d = "Shah";
+        String e = "Virdi";
+        String g = "Baaz";
+        String res = d.concat(e).concat(g);
+        System.out.println(res);
+        System.out.println(10+50);
+        for(int k =1;k<=10;k++){
+            System.out.println(k);
+        }
+        for(int p=0;p<=50;p+=2){
+            System.out.print(p+" ");
+        }
+        for(int w =0;w<=50;w++){
+            if(w%2!=0){
+            System.out.println(w+" ");
+            }
+        }
+        int sum = 0;
+        for(int ju =0;ju<=100;ju++){
+            sum+=ju;
+        }
+        System.out.println(sum);
+        for(int ui = 1; ui<=10;ui++){
+            //System.out.println(ui "*5="ui*5);
+        }
+        int io = 20;
+        while(io>0){
+            System.out.println(io);
+        io--;    
+        }
+        int fact =1;
+        int iou = 5;
+        while(iou>0){
+            fact*=iou;
+            iou--;
+        }System.out.println(fact);
+        int numio = 123456;
+        int count = 0;
+        while(numio!=0){
+            count++;
+            numio/=10;
+        }System.out.println(count);
+        int juio = 1;
+        do{
+            System.out.println(juio);
+            juio++;
+        }
+        while(juio<=20);
+        int huio = 2;
+        do{
+            System.out.println(huio);
+            huio++;
+        }
+        while(huio<=20);
+        int duio = 1;
+        int sumuio = 0;
+        do{
+            sumuio+=duio;
+            duio++;
+        }
+        while(duio<=50);
+        System.out.println(sumuio);
+        int fuio = 9876;
+        int rev = 0;
+        do{
+            rev = rev*10+fuio%10;
+            fuio = fuio/10;
+        }
+        while(fuio>0);
+        System.out.println(rev);
+        int yuot = 123221;
+        int temp = yuot;
+        int revio=0;
+        do{
+            revio = revio*10+temp%10;
+            temp/=10;
+        
+
+    }while(temp!=0);
+     if(revio == yuot){
+                System.out.println("Palindrome");
+            }else{
+                System.out.println("Not a palindrome");
+            }
+    int[] num = {10,20,30,40};
+    for(int iop : num){
+        System.out.println(iop);
+    }*/
+    // for(int tyui = 0; tyui<3; tyui++){
+    //     System.out.println("*");
+    // }
+    // for(int qwe=0; qwe<3; qwe++){
+    //     System.out.print("* ");
+    // }
+    // for(int fghy = 0;fghy<3;fghy++){
+    //     for(int gfhy=0;gfhy<3;gfhy++){
+    //         System.out.print("* ");
+    //     }System.out.println(" ");
+    // }
+    // for(int trym =1 ;trym <=3; trym++){
+    //     for(int trmy=1;trmy<=3; trmy++){
+    //         System.out.print(trym +" ");
+    //     }System.out.println(" ");
+    // }
+    // for(int ipu = 0;ipu<3;ipu++){
+    //     for(int jpu=ipu+1; jpu<ipu+4; jpu++){
+    //         System.out.print(jpu + " ");
+    //     }System.out.println(" ");
+    // }
+    // for(int iuot = 0 ;iuot <3;iuot++){
+    //     for(int hut = iuot+1;hut<4+iuot;hut++){
+    //         System.out.print(hut+" ");
+    //     }System.out.println(" ");
+    // }
+    // for(int ryu = 0; ryu<=3;ryu++){
+    //     for(int gyu = 1; gyu<=ryu; gyu++){
+    //         System.out.print(gyu+" ");
+    //     }System.out.println(" ");
+    // }
+    // for(int fyu = 0;fyu<=2;fyu++){
+    //     for(int jyu = 0;jyu<=fyu;jyu++){
+    //         System.out.print("*");
+    //     }System.out.println(" ");
+    // }
+    // for(int dui = 0; dui<3; dui++){
+    //     for(int fui = 0;fui<3-dui;fui++){
+    //         System.out.print("*");
+    //     }System.out.println(" ");
+    // }
+    // for(int qw =3; qw>0; qw--){
+    //     for(int yw = 0;yw<3;yw++){
+    //         System.out.print(qw+" ");
+    //     }System.out.println(" ");
+    // }
+    // for(int kut = 0;kut<3;kut++){
+    //     for(int jut = 0; jut<kut+1;jut++){
+    //         System.out.print(kut+1+" ");
+    //     }System.out.println(" ");
+    // }
+    // int x = 1;
+    // for(int iop = 0;iop<4;iop++){
+    //     for(int jop = 0;jop<iop;jop++){
+    //         System.out.print(x+" ");
+    //         x++;
+    //     }System.out.println();
+    // }
+    // int y = 1;
+    // for(int i = 0;i<3;i++){
+    //     for(int j =0 ;j<3;j++){
+    //         System.out.print(y+" ");
+    //         y++;
+    //     }System.out.println(" ");
+    // }
 }
-//This is my first Java code.
+
+
+    
+}
+
 /* I am learning Java to become a 
 Backend and 
     Full Stack developer */
