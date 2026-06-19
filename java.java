6679867576f9e8d53969cc08304e56a6903dafd9
Sh2007v <virdisha50@gmail.com>
2026-06-19 19:04:58 +0530

    k

--- java.java
+++ java.java
@@ -440,13 +440,15 @@ public class java{
 //         }
 //     }
 // }
-int x = 1;
-for(int i=0;i<n;i++){
-    for(int j=0;j<i;j++){
-        System.out.print(x+" ");
-        x++;
-    }System.out.println();
-}
+// int x = 1;
+// for(int i=0;i<n;i++){
+//     for(int j=0;j<i;j++){
+//         System.out.print(x+"  ");
+//         x++;
+//     }System.out.println();
+// }
+
+
         }
     }
 
