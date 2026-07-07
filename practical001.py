046ae33e171996d91fcd1e710c5b7ce4b0e1f3ae
Sh2007v <virdisha50@gmail.com>
2026-07-07 08:24:48 +0530

    l

---
+++ practical001.py
@@ -0,0 +1,13 @@
+#Run this cell first - it loads all the libraries we need
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+#Makes plots looj clean
+
+plt.rcParams['figure.figsize'] = (10,5)
+plt.rcParams['font.size']
+
+# POPULATION
+population = [162,175,178,567,345]
+student_ids = ['S1','S2','S3','S4','S5']
