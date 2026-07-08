b5447cf51b401ef020e45a632017cea57b62750d
Sh2007v <virdisha50@gmail.com>
2026-07-08 20:17:00 +0530

    l

--- arrays_rev.java
+++ arrays_rev.java
@@ -109,3 +109,32 @@
 //     }
 // }
 
+
+//Sum and average of an array
+// import java.util.Scanner;
+// public class arrays_rev{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter size of array : ");
+//         int n = sc.nextInt();
+
+//         sc.nextLine();
+
+//         int[] arr = new int[n];
+
+//         System.out.println("Enter the array :");
+        
+//         for(int i=0;i<n;i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         int sum = 0;
+        
+//             for(int i=0;i<n;i++){
+//                 sum+=arr[i];
+//             }
+//         System.out.println("Sum : "+sum);
+//     }
+// }
+
+
