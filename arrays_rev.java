654b566c6bdb83a89216b097a425567ed3bd4ee2
Sh2007v <virdisha50@gmail.com>
2026-07-10 23:30:47 +0530

    Continuing Arrays Revision

--- arrays_rev.java
+++ arrays_rev.java
@@ -202,3 +202,91 @@
 //     }
 // }
 
+
+
+//PLACE ALL ZEROES AT THE END OF AN ARRAY
+
+// import java.util.Scanner;
+// public class arrays_rev{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Size of Array :");
+//         int size = sc.nextInt();
+
+//         int[] arr = new int[size];
+
+//         System.out.println("Enter the Array :");
+//         for(int i=0;i<size;i++){
+//             arr[i] = sc.nextInt();
+//         }
+//         for(int i=0;i<=size;i++){
+//             if(arr[i]==0 && arr[i+1]!=0){
+//                 int temp = arr[i+1];
+//                 arr[i+1] = arr[i];
+//                 arr[i] = temp;
+//             }
+//         }
+//         for(int i=0;i<size;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+
+
+/////////////////////////
+///   2d arrays /////////
+/////////////////////////
+/// 
+/// 
+/// 
+/// ROW SUM
+// import java.util.Scanner;
+// public class arrays_rev{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Size of Array :");
+//         int size = sc.nextInt();
+
+//         int[][] arr = new int[size][size];
+
+//         System.out.println("Enter the Array :");
+        
+//         for(int i=0;i<size;i++){
+//             for(int j=0;j<size;j++){
+//                 arr[i][j] = sc.nextInt();
+//         }
+//     }
+//         for(int i=0;i<size;i++){
+//             int row_sum = 0;
+//             for(int j =0;j<size;j++){
+//                 row_sum+=arr[i][j];
+//             }
+//             System.out.print(row_sum+" ");
+//             System.out.println();
+//         }
+//     }
+// }
+
+///COLUMN SUM
+import java.util.Scanner;
+public class arrays_rev{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the rows num :");
+        int row = sc.nextInt();
+
+        System.out.println("Enter the cols num :");
+        int cols = sc.nextInt();
+
+        int[][] arr = new int[row][cols];
+
+        for(int i=0;i<row;i++){
+            for(int j =0;j<cols;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+    }
+}
