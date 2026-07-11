b4bf9df9d038864b46e728cf1589efb4c230d1ac
Sh2007v <virdisha50@gmail.com>
2026-07-11 14:48:39 +0530

    New file

---
+++ Java/Fundamentals/Arrays/MultiDimensionalArrays/practice.java
@@ -0,0 +1,55 @@
+package Java.Fundamentals.Arrays.MultiDimensionalArrays;
+
+public class practice  {
+    // ROW SUM
+// import java.util.Scanner;
+// public class arrays_rev{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Size of Array :");
+//         int size = sc.nextInt();
+
+//         int[][] arr = new int[size][size];
+
+//         System.out.println("Enter the Array :");
+        
+//         for(int i=0;i<size;i++){
+//             for(int j=0;j<size;j++){
+//                 arr[i][j] = sc.nextInt();
+//         }
+//     }
+//         for(int i=0;i<size;i++){
+//             int row_sum = 0;
+//             for(int j =0;j<size;j++){
+//                 row_sum+=arr[i][j];
+//             }
+//             System.out.print(row_sum+" ");
+//             System.out.println();
+//         }
+//     }
+// }
+
+///COLUMN SUM
+// import java.util.Scanner;
+// public class arrays_rev{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter the rows num :");
+//         int row = sc.nextInt();
+
+//         System.out.println("Enter the cols num :");
+//         int cols = sc.nextInt();
+
+//         int[][] arr = new int[row][cols];
+
+//         for(int i=0;i<row;i++){
+//             for(int j =0;j<cols;j++){
+//                 arr[i][j] = sc.nextInt();
+//             }
+//         }
+//     }
+// }
+    
+}
