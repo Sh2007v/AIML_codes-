4eba1bdaa10a5471817f61bf6afcc9021dc0f239
Sh2007v <virdisha50@gmail.com>
2026-07-13 12:44:40 +0530

    Updated File

--- Java/OOP/Inheritance/SingleInheritance/examples.java
+++ Java/OOP/Inheritance/SingleInheritance/examples.java
@@ -430,6 +430,193 @@ package Java.OOP.Inheritance.SingleInheritance;
 
 
 
+// import java.util.Scanner;
+
+// class Vehicle{
+
+//     protected String brand;
+
+//     public void setVehicle(String brand){
+
+//         this.brand = brand;
+//     }
+
+//     public void getVehicle(){
+
+//         System.out.println("Brand : "+brand);
+
+//     }
+
+// }
+
+// class Car extends Vehicle{
+
+//     protected String model;
+
+//     public void setCar(String model){
+
+//         this.model = model;
+
+//     }
+//     public void getCar(){
+
+//         System.out.println("Model : "+model);
+
+//     }
+// }
+
+// class ElectricCar extends Car{
+
+//     private int batteryCapacity;
+
+//     public void setElectricCar(int batteryCapacity){
+
+//         if(batteryCapacity<20){
+
+//             this.batteryCapacity = batteryCapacity;
+
+//         }
+//     }
+
+//     public void getElectricCar(){
+
+//         System.out.println("Battery : "+ batteryCapacity);
+
+//     }
+// }
+
+// public class examples{
+
+//     public static void main(String[] args){
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter Brand :");
+//         String brand = sc.nextLine();
+
+//         System.out.println("Model : ");
+//         String model = sc.nextLine();
+
+//         System.out.println("Enter Battery Capacity : ");
+//         int batteryCapacity = sc.nextInt();
+
+//         ElectricCar e = new ElectricCar();
+
+//         e.setVehicle(brand);
+//         e.setCar(model);
+//         e.setElectricCar(batteryCapacity);
+
+//         System.out.println();
+        
+//         e.getVehicle();
+//         e.getCar();
+//         e.getElectricCar();
+//     }
+// }
+
+
+
+
+// import java.util.Scanner;
+
+// class Account{
+
+//     protected int accountNumber;
+
+//     public void setaccount(int accountNumber){
+
+//         this.accountNumber = accountNumber;
+
+//     }
+
+//     public void displayaccount(){
+
+//         System.out.println("Account Number : "+accountNumber);
+
+//     }
+
+// }
+
+// class SavingsAccount extends Account{
+
+//     protected int balance;
+
+//     public void setbalance(int balance){
+
+//         if(balance<1000){
+
+//             this.balance = 1000;
+
+//         }
+//         else{
+
+//             this.balance = balance;
+
+//         }
+//     }
+
+//     public void displaybalance(){
+
+//         System.out.println("Balance : "+balance);
+//     }
+// }
+
+// class PremiumSavings extends SavingsAccount{
+
+//     private int interestRate;
+
+//     public void setinterestRate(int interestRate){
+
+//         if(interestRate<4){
+
+//             this.interestRate = 4;
+
+//         }else{
+
+//             this.interestRate = interestRate;
+//         }
+//     }
+
+//     public void displayinterestRate(){
+
+//         System.out.println("Interest Rate : "+interestRate+"%");
+
+//     }
+// }
+
+// public class examples{
+
+//     public static void main(String[] args){
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter Account Number : ");
+//         int accountNumber = sc.nextInt();
+
+//         sc.nextLine();
+
+//         System.out.println("Enter Balance : ");
+//         int balance = sc.nextInt();
+
+//         sc.nextLine();
+
+//         System.out.println("Enter Interest Rate : ");
+//         int interestRate = sc.nextInt();
+
+//         PremiumSavings p = new PremiumSavings();
+
+//         p.setaccount(accountNumber);
+//         p.setbalance(balance);
+//         p.setinterestRate(interestRate);
+
+//         p.displayaccount();
+//         p.displaybalance();
+//         p.displayinterestRate();        
+
+        
+
+//     }
+// }
 
 
 
