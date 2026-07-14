cbd8c975362e0ace4416fc6c77d3e85fec7fa368
Sh2007v <virdisha50@gmail.com>
2026-07-14 12:29:48 +0530

    Upadted file

---
+++ Java/OOP/Classes/examples.java
@@ -0,0 +1,32 @@
+// class Calculator{
+//     int n1;
+//     int n2;
+// }
+// public int calcsum(int n1, int n2){
+//     return n1+n2;
+// }
+
+// public int clasdifference(int n1, int n2){
+//     if(n2>n1){
+//         return n2-n1;
+//     }
+//     else{
+//         return n1-n2;
+//     }
+// }
+
+// public class examples{
+//     public static void main(String[] args){
+
+//         Scanner sc = new Scanner(System.in);
+
+//         // calcsum(n1,n2) c = new calcsum();
+//         System.out.println("Enter the 1st num : ");
+//         int n1 = sc.nextInt();
+
+//         System.out.println("Enter the second num :");
+//         int n2 = sc.nextInt();
+
+//         // System.out.println("Sum : " + calcsum(n1,n2));
+//     }
+// }
