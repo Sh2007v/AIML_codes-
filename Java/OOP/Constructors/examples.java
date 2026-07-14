8abb4ee891d99fe4cd26ae981964f0421e34fb47
Sh2007v <virdisha50@gmail.com>
2026-07-14 22:13:18 +0530

    New File

--- Java/OOP/Constructors/examples.java
+++ Java/OOP/Constructors/examples.java
@@ -1,47 +1,47 @@
-package Java.OOP.Constructors;
+// package Java.OOP.Constructors;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-class Rectangle{
+// class Rectangle{
 
-        private int length;
-        private int breadth;
+//         private int length;
+//         private int breadth;
 
-        public void setRectangle(int length, int breadth){
-            if(length>=0){
-                this.length = length;
-            }
-            else{
-                this.length = 1;
-            }
+//         public void setRectangle(int length, int breadth){
+//             if(length>=0){
+//                 this.length = length;
+//             }
+//             else{
+//                 this.length = 1;
+//             }
 
-            if(breadth>=0){
-                this.breadth = breadth;
-            }
-            else{
-                this.breadth = 1;
-            }
-    }
-        public void getRectangle(){
+//             if(breadth>=0){
+//                 this.breadth = breadth;
+//             }
+//             else{
+//                 this.breadth = 1;
+//             }
+//     }
+//         public void getRectangle(){
 
-            System.out.println("Area : "+(length*breadth));
+//             System.out.println("Area : "+(length*breadth));
         
-        }
-}
+//         }
+// }
 
-public class examples{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+// public class examples{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
 
-        int length = sc.nextInt();
-        sc.nextLine();
+//         int length = sc.nextInt();
+//         sc.nextLine();
         
-        int breadth = sc.nextInt();
+//         int breadth = sc.nextInt();
 
-        Rectangle r = new Rectangle();
+//         Rectangle r = new Rectangle();
 
-        r.setRectangle(length, breadth);
-        r.getRectangle();
-    } 
-}
+//         r.setRectangle(length, breadth);
+//         r.getRectangle();
+//     } 
+// }
 
