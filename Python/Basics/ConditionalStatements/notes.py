5ff14017273cc28a9dddc1089ad8a38a022968aa
Sh2007v <virdisha50@gmail.com>
2026-07-16 09:23:06 +0530

    New Updated

---
+++ Python/Basics/ConditionalStatements/notes.py
@@ -0,0 +1,12 @@
+# ConditionalStatements
+
+# if condition:
+#      code runs if condition is True
+
+# elif another_condition:
+#      code runs if previous condition was False
+#      and this condition is True
+
+# else:
+#      code runs if all conditions are False
+
