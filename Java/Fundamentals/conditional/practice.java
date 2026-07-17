a5a029a274e3ac5988134c4a541baaba8a937faf
Sh2007v <virdisha50@gmail.com>
2026-07-17 05:58:17 +0530

    New File

---
+++ Java/Fundamentals/conditional/practice.java
@@ -0,0 +1,60 @@
+package Java.Fundamentals.conditional;
+
+import java.util.Scanner;
+
+
+// EVEN OR ODD
+// public class practice {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+
+//         if(n%2==0){
+//             System.out.println("Even");
+//         }
+//         else{
+//             System.out.println("Odd");
+//         }
+//     }
+// }
+
+
+// SALARY + BONUS
+//  public class practice {
+//   public static void main(String[] args){
+//          Scanner sc = new Scanner(System.in);
+
+//         int empID = sc.nextInt();
+//         sc.nextLine();
+//         String empName = sc.nextLine();
+//         int basSalary = sc.nextInt();
+//         sc.nextLine();
+//         int yearsOfexp = sc.nextInt();
+
+//         int finalSal = 0;
+//         int bonus = 0;
+
+//         if(yearsOfexp<2){
+//             // bonus = basSalary*(5/100);    //integer division - 0;
+//             bonus = basSalary*5/100;
+//         }
+//         else if(yearsOfexp>=2 && yearsOfexp<=5){
+//             // bonus = basSalary*(1/10);
+//             bonus = basSalary*1/10;
+//         }
+//         else{
+//             // bonus = basSalary*(2/10);
+//             bonus = basSalary*2/10;
+//         }
+
+
+//         finalSal = basSalary + bonus;
+
+//         System.out.println("Bonus : "+bonus);
+//         System.out.println("Final Salary : "+finalSal);
+//     }
+// }
+
+
+
