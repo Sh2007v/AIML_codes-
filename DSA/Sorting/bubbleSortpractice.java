4cd7313b0cb8cb42523f58dbbd12cfb166d2366c
Sh2007v <virdisha50@gmail.com>
2026-07-28 21:15:29 +0530

    New File

---
+++ DSA/Sorting/bubbleSortpractice.java
@@ -0,0 +1,69 @@
+package DSA.Sorting;
+
+import java.util.Scanner;
+// 1 . BUBBLE SORT IN ASCENDING ORDER
+
+public class bubbleSortpractice {
+
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter the size : ");
+//         int size = sc.nextInt();
+
+//         int[] arr = new int[size];
+
+//         for(int i=0;i<size;i++){
+//             arr[i] = sc.nextInt();
+//         }
+
+//         for(int i=0;i<size;i++){
+//             for(int j=0;j<size-i-1;j++){
+//                 if(arr[j]>arr[j+1]){
+                    
+//                     int temp = arr[j];
+//                     arr[j] = arr[j+1];
+//                     arr[j+1] = temp;
+                
+//                 }
+//             }
+//         }
+
+//         for(int i=0;i<size;i++){
+//             System.out.println(arr[i]+" ");
+//         }
+
+
+//     }   
+
+// public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.println("Enter the size: ");
+//     int size = sc.nextInt();
+
+//     int[] arr = new int[size];
+
+//     for(int i=0;i<size;i++){
+//         arr[i] = sc.nextInt();
+//     }
+
+//     for(int i=0;i<size;i++){
+//         for(int j=0;j<size-i-1;j++){
+//             if(arr[j]<arr[j+1]){
+                
+//                 int temp = arr[j+1];
+//                 arr[j+1] = arr[j];
+//                 arr[j] = temp;
+
+//             }
+//         }
+//     }
+
+//     for(int i=0;i<arr.length;i++){
+//         System.out.print(arr[i]+" ");
+//     }
+// }
+// }
+}
+
