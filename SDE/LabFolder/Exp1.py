311e313110b8bc6244f0cc947f08afbed85f186d
Sh2007v <virdisha50@gmail.com>
2026-08-06 12:53:33 +0530

    New FIle

---
+++ SDE/LabFolder/Exp1.py
@@ -0,0 +1,6 @@
+# To perform EDA on a dataset using the Pandas library to understand the structure, quality and 
+# statistical properties of data
+
+import pandas as pd
+df = pd.read_excel("AIML_codes-/SDE/LabFolder/Students.xlsx ")
+print(df)
