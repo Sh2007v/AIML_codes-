a717e07ff0258140a515dd63d0fcce16d0774d0b
Sh2007v <virdisha50@gmail.com>
2026-08-10 20:19:35 +0530

    New FIle

--- Java/Fundamentals/Strings/practice.java
+++ Java/Fundamentals/Strings/practice.java
@@ -1245,4 +1245,24 @@
         
 //     }
 // }
+
+
+//STRING BUILDER 
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the String : ");
+        String str = sc.nextLine();
+
+        StringBuilder sb = new StringBuilder(str);
+
+        sb.reverse();
+
+        System.out.println("Reversed String : " + sb);
+    }
+}
 // 
