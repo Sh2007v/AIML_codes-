47b3e03a7d8a1a53084c47b85af849254727bc28
Sh2007v <virdisha50@gmail.com>
2026-08-11 21:17:22 +0530

    New File

--- Java/Assignments/Assignment1.java
+++ Java/Assignments/Assignment1.java
@@ -3,41 +3,46 @@ package Java.Assignments;
 import java.util.Scanner;
 
 public class Assignment1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
 
-        //hardcode values : row : 3; col : 5;
+//         //hardcode values : row : 3; col : 5;
 
-        int rows = 3;
-        int cols = 5;
-        //int[][] arr = new int[rows][cols];
+//         int rows = 3;
+//         int cols = 5;
+//         //int[][] arr = new int[rows][cols];
 
-        // System.out.println("Enter the array : ");
-        // for(int i=0;i<rows;i++){
-        //     for(int j=0;j<cols;j++){
-        //         arr[i][j]=sc.nextInt();
-        //     }
-        // }
-        int[][] arr = {
-                {0, 1, 1, 1, 1},
-                {1, 0, 1, 1, 1},
-                {1, 1, 0, 1, 1}
-        };
-        System.out.println("Enter the columns num : ");
-        int col = sc.nextInt();
+//         // System.out.println("Enter the array : ");
+//         // for(int i=0;i<rows;i++){
+//         //     for(int j=0;j<cols;j++){
+//         //         arr[i][j]=sc.nextInt();
+//         //     }
+//         // }
+//         int[][] arr = {
+//                 {0, 1, 1, 1, 1},
+//                 {1, 0, 1, 1, 1},
+//                 {1, 1, 0, 1, 1}
+//         };
+//         System.out.println("Enter the columns num : ");
+//         int col = sc.nextInt();
 
-        int max_sum = 0;
-        int sum = 0;
+//         int max_sum = 0;
+//         int sum = 0;
 
-        for(int i=0;i<rows;i++){
-            if(arr[i][col]==0){
-                max_sum = sum;
-                break;
-            }
-            else{
-                sum+=arr[i][col];
-            }
-        }
-        System.out.println("Max Sum : "+max_sum);
-    }
+//         for(int i=0;i<rows;i++){
+//             if(arr[i][col]==0){
+//                 max_sum = sum;
+//                 break;
+//             }
+//             else{
+//                 sum+=arr[i][col];
+//             }
+//         }
+//         System.out.println("Max Sum : "+max_sum);
+//     }
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    
+}
 }
