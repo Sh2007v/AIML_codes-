1d403183eafb10c8240396b14c028cda56391076
Sh2007v <virdisha50@gmail.com>
2026-08-12 21:50:50 +0530

    New File

--- SDE/LabFolder/Exp1.py
+++ SDE/LabFolder/Exp1.py
@@ -2,5 +2,14 @@
 # statistical properties of data
 
 import pandas as pd
-df = pd.read_excel("AIML_codes-/SDE/LabFolder/Students.xlsx ")
-print(df)
+#df = pd.read_excel("AIML_codes-/SDE/LabFolder/Students.xlsx ")
+#print(df)
+df1 = pd.read_excel("AIML_codes-/SDE/LabFolder/student.xlsx")
+print(" ")
+print(df1)
+print(df1.shape)
+print(df1.columns)
+print(df1.dtypes)
+print(df1.info())
+print(df1.describe(include='all'))
+
