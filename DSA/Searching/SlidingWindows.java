e8450a0f9bc08fddd986ff60ebc71dabb7178210
Sh2007v <virdisha50@gmail.com>
2026-08-14 22:11:39 +0530

    New File

--- DSA/Searching/SlidingWindows.java
+++ DSA/Searching/SlidingWindows.java
@@ -1,31 +1,31 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class SlidingWindows{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+// public class SlidingWindows{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
 
-        int size = sc.nextInt();
+//         int size = sc.nextInt();
 
-        int[] arr = new int[size];
+//         int[] arr = new int[size];
 
         
-        for(int i=0;i<size;i++){
-            arr[i] = sc.nextInt();
-        }
+//         for(int i=0;i<size;i++){
+//             arr[i] = sc.nextInt();
+//         }
 
-        int max = 0;
+//         int max = 0;
 
-        for(int i=0;i<=size-3;i++){
-            int sum = 0;
-            for(int j=i;j<i+3;j++){
+//         for(int i=0;i<=size-3;i++){
+//             int sum = 0;
+//             for(int j=i;j<i+3;j++){
                 
-            sum += arr[j] ;
+//             sum += arr[j] ;
             
-        }
-        if(sum>max){
-            max = sum;
-        }
-        }
-        System.out.println(max);
-    }
-}
+//         }
+//         if(sum>max){
+//             max = sum;
+//         }
+//         }
+//         System.out.println(max);
+//     }
+// }
