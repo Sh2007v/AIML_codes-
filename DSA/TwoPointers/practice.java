11654a3d02f3c6a9446b7ebe2ebc91c1c5dc89b7
Sh2007v <virdisha50@gmail.com>
2026-08-15 23:50:23 +0530

    New FIle

---
+++ DSA/TwoPointers/practice.java
@@ -0,0 +1,218 @@
+package DSA.TwoPointers;
+
+import java.util.Scanner;
+// import java.util.Arrays;
+
+public class practice {
+    
+    //FINDING PAIR WITH GIVEN SUM
+    // static void findPair(int[] arr, int target){
+        
+    //     int left = 0;
+    //     int right = arr.length-1;
+        
+    //     boolean found = false;
+
+    //     while(left<right){
+    //         int sum = arr[left]+arr[right];
+
+    //         if(sum<target){
+    //             left++;
+    //         }else if(sum>target){
+    //             right--;
+    //         }else{
+    //             found = true;
+    //             //System.out.println("Pair found");
+    //             break;
+    //         }
+    //     }
+    //     if(found){
+    //         System.out.println("Pair Found");
+    //     }else{
+    //         System.out.println("Pair does not exist");
+    //     }
+    // }
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.println("Enter the size:");
+    //     int size = sc.nextInt();
+
+    //     int[] arr = new int[size];
+
+    //     System.out.println("Enter the array : ");
+    //     for(int i=0;i<size;i++){
+    //         arr[i] = sc.nextInt();
+    //     }
+
+    //     System.out.println("Enter the sum : ");
+    //     int target = sc.nextInt();
+
+    //     Arrays.sort(arr);
+    //     findPair(arr,target);
+
+    // }
+
+    //REVERSE AN ARRAY
+
+    // static void reverse(int[] arr){
+        
+    //     int left=0;
+    //     int right=arr.length-1;
+
+    //     while(left<right){
+    //     {
+    //         int temp = arr[right];
+    //         arr[right] = arr[left];
+    //         arr[left] = temp;
+
+    //     }
+    //     left++;
+    //     right--;
+    // }
+    //     for(int i=0;i<arr.length;i++){
+    //         System.out.print(arr[i]+" ");
+    //     }
+    // }
+
+
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.println("Enter the size : ");
+    //     int size = sc.nextInt();
+
+    //     int[] arr = new int[size];
+
+    //     for(int i=0;i<size;i++){
+    //         arr[i] = sc.nextInt();
+    //     }
+
+    //     reverse(arr);
+    // }
+
+    //IS GIVEN ARRAY PALINDROME
+    // static void isPalindrome(int[] arr){
+    //     int left=0;
+    //     int right = arr.length-1;
+
+    //     boolean notPal = false;
+
+    //     while(left<right){
+    //         if(arr[left] != arr[right]){
+    //             notPal = true;
+    //             break;
+    //         }
+    //         left++;
+    //         right--;
+    //     }
+    //     if(notPal){
+    //         System.out.println("Not Palindrome");
+    //     }
+    //     else{
+    //         System.out.println("Palindrome");
+    //     }
+    // }
+
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.println("Enter the size : ");
+    //     int size = sc.nextInt();
+
+    //     int[] arr = new int[size];
+
+    //     System.out.println("Enter the Array : ");
+    //     for(int i=0;i<size;i++){
+    //         arr[i] = sc.nextInt();
+    //     }
+    //     isPalindrome(arr);
+    // }
+
+    //FIND PAIR DIFFERENCE
+    // static void findpairDiff(int[] arr, int target){
+    //     int left = 0;
+    //     int right = arr.length-1;
+
+    //     boolean found = false;
+    //     while(left<right){
+    //         int diff = arr[left] - arr[right];
+
+    //         if(diff==target){
+    //             found = true;
+    //             break;
+    //         }
+    //         else if(diff<target){
+    //             left++;
+    //         }else{
+    //             right--;
+    //         }
+    //     }
+    //     if(found){
+    //         System.out.println("Pair found !");
+    //     }else{
+    //         System.out.println("No Pair found ");
+    //     }
+    // }
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+
+    //     System.out.println("Enter the size : ");
+    //     int size = sc.nextInt();
+
+    //     int[] arr = new int[size];
+
+    //     System.out.println("Enter the Array : ");
+        
+    //     for(int i=0;i<size;i++){
+    //         arr[i] = sc.nextInt();
+    //     }
+
+    //     System.out.println("Enter the required difference : ");
+    //     int target = sc.nextInt();
+
+    //     findpairDiff(arr, target);
+
+    // }
+
+    //Remove duplicate elements (most important)
+//     static void removeDuplicates(int[] arr) {
+
+//     int slow = 0;
+//     int fast = 1;
+
+//     while (fast < arr.length) {
+
+//         if (arr[slow] != arr[fast]) {
+//             slow++;
+//             arr[slow] = arr[fast];
+//         }
+
+//         fast++;
+//     }
+
+//     // Print unique elements
+//     for (int i = 0; i <= slow; i++) {
+//         System.out.print(arr[i] + " ");
+//     }
+// }
+
+// public static void main(String[] args) {
+
+//     Scanner sc = new Scanner(System.in);
+
+//     System.out.println("Enter the size:");
+//     int size = sc.nextInt();
+
+//     int[] arr = new int[size];
+
+//     System.out.println("Enter the Array:");
+//     for (int i = 0; i < size; i++) {
+//         arr[i] = sc.nextInt();
+//     }
+
+//     removeDuplicates(arr);
+
+
+//     }
+}
