9eee7dbf6813dcc6105b420abfb1d0d46945d5b2
Sh2007v <virdisha50@gmail.com>
2026-08-15 23:37:41 +0530

    New File

---
+++ Java/Fundamentals/Wrapper_Classes/practice.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class practice{
+    public static void main(String[] args){
+
+        int x = 10;
+
+        Integer obj = x;//auto-boxing
+        int y = obj;    //unboxing
+
+        System.out.println(obj);
+        System.out.println(y);
+
+    }
+}
