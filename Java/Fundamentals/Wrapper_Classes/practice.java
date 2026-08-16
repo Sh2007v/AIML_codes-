78ee384ed4339d8bd3406d36b1efa666ad23af43
Sh2007v <virdisha50@gmail.com>
2026-08-16 22:32:22 +0530

    New File

--- Java/Fundamentals/Wrapper_Classes/practice.java
+++ Java/Fundamentals/Wrapper_Classes/practice.java
@@ -1,15 +1,43 @@
 import java.util.Scanner;
 
 public class practice{
-    public static void main(String[] args){
+//     public static void main(String[] args){
 
-        int x = 10;
+//         int x = 10;
 
-        Integer obj = x;//auto-boxing
-        int y = obj;    //unboxing
+//         Integer obj = x;//auto-boxing
+//         int y = obj;    //unboxing
 
-        System.out.println(obj);
-        System.out.println(y);
+//         System.out.println(obj);
+//         System.out.println(y);
 
-    }
+        // static void findLarger(int num1, int num2){
+        //     if(num1>num2){
+        //         System.out.println(num1);
+        //     }else{
+        //         System.out.println(num2);
+        //     }
+        // }
+
+        // public static void main(String[] args){
+        //     Scanner sc = new Scanner(System.in);
+
+        //     System.out.println("Enter 1st num : ");
+        //     String Str1 = sc.nextLine();
+
+        //     System.out.println("Enter 2nd num : ");
+        //     String Str2 = sc.nextLine();
+
+        //     int num1 = Integer.parseInt(Str1);
+        //     int num2 = Integer.parseInt(Str2);
+
+        //     findLarger(num1, num2);
+        // }
+
+        //CORE PATTERN
+        // String input = sc.nextLine();
+        // int num = Integer.parseInt(input);
+        // methodName(num);
+//     }
+
 }
