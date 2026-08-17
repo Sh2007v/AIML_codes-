f3c2aab029d1f6c383ca80957b3679668df1ab70
Sh2007v <virdisha50@gmail.com>
2026-08-17 21:21:20 +0530

    New File

--- DSA/TwoPointers/practice.java
+++ DSA/TwoPointers/practice.java
@@ -298,6 +298,46 @@ public class practice {
 //     removeElem(arr, x);    
 // }
 
+//MERGE TWO SORTED ARRAYS
+
+// static void mergeArrays(int[] arr1, int[] arr2){
+
+//     int i = 0;
+//     int j = 0;
+//     int k = 0;
+
+//     int[] result = new int[arr1.length + arr2.length];
+
+//     while(i < arr1.length && j < arr2.length){
+
+//         if(arr1[i] < arr2[j]){
+//             result[k] = arr1[i];
+//             i++;
+//         }
+//         else{
+//             result[k] = arr2[j];
+//             j++;
+//         }
+
+//         k++;
+//     }
+
+//     while(i < arr1.length){
+//         result[k] = arr1[i];
+//         i++;
+//         k++;
+//     }
+
+//     while(j < arr2.length){
+//         result[k] = arr2[j];
+//         j++;
+//         k++;
+//     }
+
+//     for(int x : result){
+//         System.out.print(x + " ");
+//     }
+// }
 
 
 }
