c152ae831153ec6be541072c548be72fb93b3ed0
Sh2007v <virdisha50@gmail.com>
2026-08-19 21:27:51 +0530

    New FIle

---
+++ Java/Assignments/Assignment2.java
@@ -0,0 +1,48 @@
+ package Java.Assignments;
+
+public class Assignment2 {
+    
+//     // interface Area{
+//     //     public void display();
+//     // }
+//     // class Square implements Area{
+//     //     public void display(){
+//     //         System.out.println("I am from Square");
+//     //     }
+//     // }
+//     // class Rect implements Area{
+//     //     public void display(){
+//     //         System.out.println("I am from Rectangle");
+//     //     }
+//     // }
+//     // public class Main{
+//     //     public static void main(String args[]){
+//     //         Square Obj = new Square();
+//     //         Obj.display();
+//     //         Rect Obj1 = new Rect();
+//     //         Obj1.display();
+//     //     }
+//     // }
+
+//     // class Rect implements Area{
+
+//     // }
+
+//     // public static void main(String[] args){
+//     //     Area Obj = new Square();
+//     //     Obj1.display();
+//     //     Area Obj1=new Rect();
+//     //     Obj1.display();
+//     // }
+
+//     // }
+
+interface Example{
+    public void display1();
+    public void display2();
+}
+
+
+
+
+}
