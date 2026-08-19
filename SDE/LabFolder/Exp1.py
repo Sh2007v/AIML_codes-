d5caa51a40b55f61a47bc0631b392bfb964d57d4
Sh2007v <virdisha50@gmail.com>
2026-08-19 21:30:17 +0530

    New File

--- SDE/LabFolder/Exp1.py
+++ SDE/LabFolder/Exp1.py
@@ -5,11 +5,34 @@ import pandas as pd
 #df = pd.read_excel("AIML_codes-/SDE/LabFolder/Students.xlsx ")
 #print(df)
 df1 = pd.read_excel("AIML_codes-/SDE/LabFolder/student.xlsx")
+# print(" ")
+# print(df1)
+# print(df1.shape)
+# print(df1.columns)
+# print(df1.dtypes)
+# print(df1.info())
+# print(df1.describe(include='all'))
+
+print(df1.isnull().sum())
+print(df1.dropna(inplace=True))
+print(df1.fillna(0))
+print(df1.fillna(df1.mean(numeric_only=True),inplace=True))
+print(df1.duplicated().sum())
+print(df1.drop_duplicates(inplace=True))
+
+df_2 = df1[['RollNo','Marks1']]
+print(df_2)
 print(" ")
-print(df1)
-print(df1.shape)
-print(df1.columns)
-print(df1.dtypes)
-print(df1.info())
-print(df1.describe(include='all'))
+print(df1['Marks1']>90)
+print(df1[df1['Marks1']>90])
+print(df1[df1['Marks1']>80])
+print(df1[df1['Group']=='G1'])
+print(" ")
+print(df1.sort_values(by='Marks1'))
+print(" ")
+print(df1.sort_values(by='Marks1',ascending=False))
+
+
+
+
 
